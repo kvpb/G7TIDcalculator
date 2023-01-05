20919c0eb4bb1929ef42a1aed3a947d9f854daf2
Karl <local-part@domain>
2023-01-05 11:10:12 +0100

    (src/)main.c updated: support outputting TSV and TRV.

--- src/main.c
+++ src/main.c
@@ -20,9 +20,12 @@ int main( int count_argument, char* vector_argument[] )
 	int16_t integer_G7SID; // G7SID: 0 < i_G7SID < 4294, i_G7SID = floor( n / 10^( l_max - 6 ) )
 	int32_t integer_G7TID; // G7TID: 0 < i_G7TID < 999999, i_G7TID = n mod 10^6
 	uint32_t integer_IDNo; // G7ID ('IDNo'): 0 < n_IDNo < 4294967295, n = i_TID + ( i_SID * 2^16 )
+	uint16_t integer_TSV; // TSV: 0 < i_TSV < 2^16 - 1, i_TSV = ( i_TID XOR i_SID ) >> 4
+	uint8_t integer_TRV; // TRV: 0 < i_TRV < 2^16 - 1, i_TRV = ( i_TID XOR i_SID ) & 0xF
 	uint16_t integer_SID; // SID: 0 < i_SID < 65535, ( 2^32 - ( 65535 * 2^16 ) ) - 1 == max_SID
 	uint16_t integer_TID; // TID: 0 < i_TID < 65535, ( 2^32 - ( 65535 * 2^16 ) ) - 1 == max_TID
 	//uint16_t max_16bitunsigned; // 16-bit unsigned maximum value: 65535
+	uint16_t result; // i_TID XOR i_SID
 	int32_t million; // 'mask': 10^6 == 1000000
 	uint32_t max_32bitunsigned; // 32-bit unsigned maximum value: 4294967295
 	int8_t base /*radix*/; // positional numeral system: decimal system ( radix = 10 )
@@ -33,6 +36,9 @@ int main( int count_argument, char* vector_argument[] )
 	integer_SID = (uint16_t) strtol(vector_argument[ 2 ], (char**) NULL, 10 );
 	integer_TID = (uint16_t) strtol(vector_argument[ 1 ], (char**) NULL, 10 );
 	//max_16bitunsigned = (uint16_t) ( pow( 2, 16 ) - 1 );
+	result = integer_TID ^ integer_SID;
+	integer_TSV = (uint16_t) ( ( result ) >> 4 );
+	integer_TRV = (uint8_t) ( ( result ) & 0xF );
 	million = (int32_t) pow( 10, 6 );
 	max_32bitunsigned = (uint32_t) ( pow( 2, 32 ) - 1 );
 	base = (int8_t) 10;
@@ -43,7 +49,7 @@ int main( int count_argument, char* vector_argument[] )
 	integer_G7TID = (int32_t) ( integer_IDNo % million );
 	integer_G7SID = (int16_t) ( floor( (double) integer_IDNo / pow( 10, ( length_maximum - length_G7SID ) ) ) );
 
-	printf("TID:\t%i\nSID:\t%i\nG7TID:\t%i\nG7SID:\t%hi\n", integer_TID, integer_SID, integer_G7TID, integer_G7SID );
+	printf("TID:\t%i\nSID:\t%i\nTSV:\t%i\nTRV:\t%i\nG7TID:\t%i\nG7SID:\t%hi\n", integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7TID, integer_G7SID );
 
 	return 0;
 }
